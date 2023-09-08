787a132c9bcc368baf4461cbbe71af829e18135e
surafel fekadu <surafelfekadu399@gmail.com>
2023-09-08 18:14:09 +0300

    creating the database and designing the models

--- BE/server.ts
+++ BE/server.ts
@@ -1,1 +1,18 @@
-// temporary file
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+// Initialize the express engine
+const app: express.Application = express();
+ 
+// Take a port 3000 for running server.
+const port = process.env.PORT || 3000;
+ 
+// Handling Request
+app.get('/', (_req, _res) => {
+    _res.send("TypeScript With Express");
+});
+ 
+// Server setup
+app.listen(port, () => {
+    console.log(`Engine running on http://localhost:${port}/`);
+});
